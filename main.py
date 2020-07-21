cf47eafe461b68977e34b258e1e102d0db40a0db
boko999 <ho-oh999@hotmail.com>
2020-07-21 17:21:38 +0000

    added stuff

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-first_name = input("hello, what is your first name?")
-last_name = input(" what is your last name?")
+# first_name = input("hello, what is your first name?")
+#last_name = input(" what is your last name?")
 
-answer = input(last_name +" "+  first_name)
+# answer = input(last_name +" "+  first_name)
+
+
+for i in range(20): 
+     if (i < 10):
+      print("less than 10")
+     else:
+      print("greater than 10")
